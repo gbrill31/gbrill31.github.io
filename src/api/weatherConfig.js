160d8ee3667545380453aa62f601080ca77d9d4f
Guy Brill <brill.guy1@gmail.com>
2019-12-30 11:58:51 +0200

    added weather config file installed fontawesome

---
+++ src/api/weatherConfig.js
@@ -0,0 +1,7 @@
+const config = {
+  key: 'ZkGkoUAQQ2TbiQnAFghvfXctA1cDsxTT'
+};
+
+export const apiRequest = {
+  autocomplete: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${config.key}`
+};
