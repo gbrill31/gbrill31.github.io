514ff74b436f350d57faecd13ea9754de0adeda7
Guy Brill <brill.guy1@gmail.com>
2020-01-08 12:27:55 +0200

    for deploy

--- src/weatherapi/weatherConfig.js
+++ src/weatherapi/weatherConfig.js
@@ -3,10 +3,10 @@ const config = {
 };
 
 const apiRequests = {
-  autocomplete: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete`, //?apikey&q=city key
-  currentConditions: `http://dataservice.accuweather.com/currentconditions/v1/`, //city key?apikey
-  forecast: `http://dataservice.accuweather.com/forecasts/v1/daily/5day/`, //city key?apikey&metric=true
-  geolocation: 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'//?apikey&q=lat,lan
+  autocomplete: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete`, //?apikey&q=city key
+  currentConditions: `https://dataservice.accuweather.com/currentconditions/v1/`, //city key?apikey
+  forecast: `https://dataservice.accuweather.com/forecasts/v1/daily/5day/`, //city key?apikey&metric=true
+  geolocation: 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search'//?apikey&q=lat,lan
 };
 
 export {
